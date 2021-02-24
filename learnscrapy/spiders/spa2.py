191cb7d4befa26e21e92854fa0e2aef1731c2dcb
周mc新国 <15886638301@163.com>
2021-02-24 16:28:31 +0800

    提交全部代码

--- learnscrapy/spiders/spa2.py
+++ learnscrapy/spiders/spa2.py
@@ -1,13 +1,21 @@
 import scrapy
 from scrapy import Request
 from learnscrapy.items import SPA1Item
+import time,random,base64
+from hashlib import sha1
 
-class SSR1(scrapy.Spider):
-    name = 'spa1'
+class SPA2Spider(scrapy.Spider):
+    name = 'spa2'
+
+    @staticmethod
+    def sign(url):
+        t = str(int(time.time()) - random.randint(1,20))
+        s = ','.join([url,t])
+        return base64.b64encode(','.join([sha1(s.encode()).hexdigest(),t]).encode()).decode()
 
     def start_requests(self):
         urls = [
-            f'https://spa1.scrape.center/api/movie/?limit=10&offset={a}' for a in range(0,100,10)
+            f'http://spa2.scrape.center/api/movie/?limit=10&offset={a}&token={self.sign("/api/movie")}' for a in range(0,100,10)
         ]
         for url in urls:
             yield scrapy.Request(url=url,callback=self.parse)
@@ -22,8 +30,10 @@ class SSR1(scrapy.Spider):
             item['country'] = '、'.join(a['regions'])
             item['time'] = a['minute']
             item['date'] = a['published_at']
-            url = response.urljoin(f'/api/movie/{a["id"]}')
-            yield Request(url=url,callback=self.parse_person,meta={'item':item})
+            s = 'ef34#teuq0btua#(-57w1q5o5--j@98xygimlyfxs*-!i-0-mb'
+            detail = base64.b64encode((s + str(a["id"])).encode()).decode()
+
+            yield Request(url=response.urljoin(f'/api/movie/{detail}/?token={self.sign("/api/movie/" + detail)}'),callback=self.parse_person,meta={'item':item})
         pass
 
     def parse_person(self,response):
